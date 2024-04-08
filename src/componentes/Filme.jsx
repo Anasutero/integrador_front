0e71f2e36518b38d8e3e63f65dacfa44fdb0f261
Ana M���ri <48643119865@>
2024-04-08 16:36:56 -0300

    atualizaçoes do projeto filmes

--- src/componentes/Filme.jsx
+++ src/componentes/Filme.jsx
@@ -1,16 +1,14 @@
 import estilos from './Filme.module.css'
 import {Card} from './Card'
 
-import poster1 from "../assets/poster1.jpg"
-
-export function Filme(){
+export function Filme({propsFilme}){
     return(
         <Card>
         <figure className={estilos.conteiner}>
-            <img src={poster1}/>
-            <figcaption>Harry Potter e a Pedra Filosofal (2001)</figcaption>
-            <p>Esta é a primeira adaptação da série de best-sellers de Harry Potter, sobre um órfão que descobre que os seus pais eram feiticeiros e que ele também possui poderes mágicos. Harry Potter passou a maior parte do seu tempo debaixo da escada na casa dos Dursleys, seus tios que não gostam dele.</p>
-            <p>79%</p>
+            <img src={`https://image.tmdb.org/t/p/w400/${propsFilme.poster_path}`}/>
+            <figcaption>{propsFilme.title}</figcaption>
+            <p>{propsFilme.overview}</p>
+            <p>{propsFilme.vote_average}</p>
         </figure>
         </Card>
     )
--- src/paginas/Lista.jsx
+++ src/paginas/Lista.jsx
@@ -1,10 +1,27 @@
 import estilos from './Lista.module.css'
+import {useEffect, useState} from 'react'
 import {Filme} from '../componentes/Filme'
 
 export function Lista(){
+
+  //criando isso para armazenar os dados da api , paa nao aparecer em console
+  const [filmes, setFilmes] = useState([])
+
+    const apiKey= "73449fc36d1b1585a0573cd7b33f08d8"
+
+    useEffect(() => {
+
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&sort_by=popularity.desc`)
+        .then(resposta => resposta.json())
+        .then(dados => setFilmes(dados.results))
+
+    }, [])
+
     return(
         <main className={estilos.conteiner}>
-          <Filme/>
+          
+          {filmes.map(umfilme => <Filme propsFilme={umfilme}/>)}
+        
       </main>
     
     )
